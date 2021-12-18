d5d993e6a120b76767793f37f99ed6c3fa167f7e
Mary <yunmaryko@gmail.com>
2021-12-19 01:05:27 +0600

    some changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var apartment = prompt('Введите ваш номер квартиры, чтобы узнать ваш подъезд');
 
 if (apartment >= 1 && apartment <= 20){
-    console.log('Вы живете в 1 подъезде');
+    alert('Вы живете в 1 подъезде');
 } else if (apartment >= 20 && apartment <= 48){
-    console.log('Вы живете в 2 подъезде');
+    calert('Вы живете в 2 подъезде');
 } else if (apartment >= 49 && apartment <= 90){
-    console.log('Вы живете в 3 подъезде');
+    alert('Вы живете в 3 подъезде');
+} else {
+    alert ('Такой квартиры в данном доме нет!');
 }
